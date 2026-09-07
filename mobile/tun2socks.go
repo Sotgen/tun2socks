6dee7ff50387d1f2c3415dd79ec56980f8add2c4
Sotgen <sotgen6@gmail.com>
2026-09-07 19:54:35 +0700

    Update tun2socks.go

--- mobile/tun2socks.go
+++ mobile/tun2socks.go
@@ -8,7 +8,7 @@ import (
 	"github.com/xjasonlyu/tun2socks/v2/core"
 	"github.com/xjasonlyu/tun2socks/v2/core/device"
 	"github.com/xjasonlyu/tun2socks/v2/core/device/fdbased"
-	_ "github.com/xjasonlyu/tun2socks/v2/proxy/socks"
+	_ "github.com/xjasonlyu/tun2socks/v2/proxy/proto/socks"
 )
 
 var (
@@ -18,9 +18,6 @@ var (
 )
 
 // StartTun2Socks menjalankan tunnel gVisor menggunakan FD dari Android VpnService
-// fd: file descriptor integer dari Android
-// socksAddr: contoh "127.0.0.1:1080"
-// mtu: standar 1500
 func StartTun2Socks(fd int, socksAddr string, mtu int) error {
 	mu.Lock()
 	defer mu.Unlock()
@@ -29,20 +26,16 @@ func StartTun2Socks(fd int, socksAddr string, mtu int) error {
 		return errors.New("tun2socks is already running")
 	}
 
-	// 1. Parameter fdbased.Open pada tun2socks v2: (name string, mtu uint32, fd int)
 	tunDev, err := fdbased.Open("tun", uint32(mtu), fd)
 	if err != nil {
 		return fmt.Errorf("failed to open fd: %w", err)
 	}
 	dev = tunDev
 
-	// 2. Format URL proxy SOCKS5
 	proxyURL := fmt.Sprintf("socks5://%s", socksAddr)
 
-	// 3. Register handler dan device ke core engine
 	core.RegisterOutputDevice(dev)
 
-	// 4. Start core engine tun2socks (gVisor stack)
 	if err := core.Start(core.WithProxy(proxyURL), core.WithStack("gvisor")); err != nil {
 		dev.Close()
 		dev = nil
